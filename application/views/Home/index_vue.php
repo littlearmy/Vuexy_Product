99e1d4b14b4581e1130a3f1c51a866191109f9bd
LittleArmy <farhan.salim2210@gmail.com>
2024-07-15 10:20:15 +0700

    15 July 2024 map not working

--- application/views/Home/index_vue.php
+++ application/views/Home/index_vue.php
@@ -12,74 +12,13 @@
  -->
 <!-- beautify ignore:start -->
 
+<?php
+$this->load->view("templates/head_vue");
+?>
 
-<html lang="en" class="light-style layout-navbar-fixed layout-menu-fixed layout-compact " dir="ltr" data-theme="theme-bordered" data-assets-path="<?= base_url('vue_assets/'); ?>" data-template="vertical-menu-template-bordered" data-style="light">
+<title><?= $judul ?></title>
 
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-
-    <title><?= $judul ?></title>
-
-    
-    <meta name="description" content="Start your development with a Dashboard for Bootstrap 5" />
-    <meta name="keywords" content="dashboard, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5">
-    <!-- Canonical SEO -->
-    <link rel="canonical" href="https://1.envato.market/vuexy_admin">
-    
-    
-    <!-- ? PROD Only: Google Tag Manager (Default ThemeSelection: GTM-5DDHKGP, PixInvent: GTM-5J3LMKC) -->
-    
-    <!-- End Google Tag Manager -->
-    
-    <!-- Favicon -->
-    <!-- <link rel="icon" type="image/x-icon" href="<?= base_url('vue_assets/'); ?>img/favicon/favicon.ico" /> -->
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap" rel="stylesheet">
-
-    <!-- Icons -->
-    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/fonts/fontawesome.css" />
-    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/fonts/tabler-icons.css"/>
-    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/fonts/flag-icons.css" />
-
-    <!-- Core CSS -->
-    
-    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/css/rtl/core.css" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/css/rtl/theme-bordered.css" class="template-customizer-theme-css" />
-    
-    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>css/demo.css" />
-    
-    <!-- Vendors CSS -->
-    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/node-waves/node-waves.css" />
-    
-    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/typeahead-js/typeahead.css" /> 
-    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/apex-charts/apex-charts.css" />
-<link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/swiper/swiper.css" />
-<link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/datatables-bs5/datatables.bootstrap5.css">
-<link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css">
-<link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css">
-<link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/leaflet/leaflet.css" />
-
-
-    <!-- Page CSS -->
-    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/css/pages/cards-advance.css" />
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-    <!-- Helpers -->
-    <script src="<?= base_url('vue_assets/'); ?>vendor/js/helpers.js"></script>
-    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-    
-    <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
-    <!-- <script src="<?= base_url('vue_assets/'); ?>vendor/js/template-customizer.js"></script> -->
-    
-    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="<?= base_url('vue_assets/'); ?>js/config.js"></script>
-    
-  </head>
+</head>
 
   <body>
 
@@ -1140,9 +1079,7 @@
                         },
                     }
                 });
-            </script>
 
-            <script>
                 const vtx = document.getElementById('myPieChart');
                 new Chart(vtx, {
                     type: 'doughnut',
@@ -1302,90 +1239,7 @@ L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
           
 
 <!-- Footer -->
-<footer class="content-footer footer bg-footer-theme">
-  <div class="container-xxl">
-    <div class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
-      <div class="text-body">
-        © <script>
-        document.write(new Date().getFullYear())
 
-        </script>, made with ❤️ by <a href="https://pixinvent.com" target="_blank" class="footer-link">Pixinvent</a>
-      </div>
-      <div class="d-none d-lg-inline-block">
-        
-        <a href="https://themeforest.net/licenses/standard" class="footer-link me-4" target="_blank">License</a>
-        <a href="https://1.envato.market/pixinvent_portfolio" target="_blank" class="footer-link me-4">More Themes</a>
-        
-        <a href="https://demos.pixinvent.com/vuexy-html-admin-template/documentation/" target="_blank" class="footer-link me-4">Documentation</a>
-        
-        
-        <a href="https://pixinvent.ticksy.com/" target="_blank" class="footer-link d-none d-sm-inline-block">Support</a>
-        
-      </div>
-    </div>
-  </div>
-</footer>
-<!-- / Footer -->
-
-          
-          <div class="content-backdrop fade"></div>
-        </div>
-        <!-- Content wrapper -->
-      </div>
-      <!-- / Layout page -->
-    </div>
-
-    
-    
-    <!-- Overlay -->
-    <div class="layout-overlay layout-menu-toggle"></div>
-    
-    
-    <!-- Drag Target Area To SlideIn Menu On Small Screens -->
-    <div class="drag-target"></div>
-    
-  </div>
-  <!-- / Layout wrapper -->
-
-
-    
-
-    
-
-    <!-- Core JS -->
-    <!-- build:js assets/vendor/js/core.js -->
-    
-    <script src="<?= base_url('vue_assets/'); ?>vendor/libs/jquery/jquery.js"></script>
-    <script src="<?= base_url('vue_assets/'); ?>vendor/libs/popper/popper.js"></script>
-    <script src="<?= base_url('vue_assets/'); ?>vendor/js/bootstrap.js"></script>
-    <script src="<?= base_url('vue_assets/'); ?>vendor/libs/node-waves/node-waves.js"></script>
-    <script src="<?= base_url('vue_assets/'); ?>vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-    <script src="<?= base_url('vue_assets/'); ?>vendor/libs/hammer/hammer.js"></script>
-    <script src="<?= base_url('vue_assets/'); ?>vendor/libs/i18n/i18n.js"></script>
-    <script src="<?= base_url('vue_assets/'); ?>vendor/libs/typeahead-js/typeahead.js"></script>
-    <script src="<?= base_url('vue_assets/'); ?>vendor/js/menu.js"></script>
-    <script src="<?= base_url('assets/'); ?>js/dashboard.js"></script>
-    
-    
-    <!-- endbuild -->
-
-    <!-- Vendors JS -->
-     
-    <script src="<?= base_url('vue_assets/'); ?>vendor/libs/chartjs/chartjs.js"></script>
-    <script src="<?= base_url('vue_assets/'); ?>vendor/libs/apex-charts/apexcharts.js"></script>
-<script src="<?= base_url('vue_assets/'); ?>vendor/libs/swiper/swiper.js"></script>
-<script src="<?= base_url('vue_assets/'); ?>vendor/libs/datatables-bs5/datatables-bootstrap5.js"></script>
-<script src="<?= base_url('vue_assets/'); ?>vendor/libs/leaflet/leaflet.js" />
-
-    <!-- Main JS -->
-    <script src="<?= base_url('vue_assets/'); ?>js/main.js"></script>
-    
-    
-    <!-- Page JS -->
-    <script src="<?= base_url('vue_assets/'); ?>js/dashboards-analytics.js"></script>
-    
-  </body>
-
-</html>
-
-<!-- beautify ignore:end -->
+<?php
+$this->load->view("templates/foot_vue");
+?>
---
+++ application/views/templates/head_vue.php
@@ -0,0 +1,65 @@
+<html lang="en" class="light-style layout-navbar-fixed layout-menu-fixed layout-compact " dir="ltr" data-theme="theme-bordered" data-assets-path="<?= base_url('vue_assets/'); ?>" data-template="vertical-menu-template-bordered" data-style="light">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+
+
+
+    <meta name="description" content="Start your development with a Dashboard for Bootstrap 5" />
+    <meta name="keywords" content="dashboard, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5">
+    <!-- Canonical SEO -->
+    <link rel="canonical" href="https://1.envato.market/vuexy_admin">
+
+
+    <!-- ? PROD Only: Google Tag Manager (Default ThemeSelection: GTM-5DDHKGP, PixInvent: GTM-5J3LMKC) -->
+
+    <!-- End Google Tag Manager -->
+
+    <!-- Favicon -->
+    <!-- <link rel="icon" type="image/x-icon" href="<?= base_url('vue_assets/'); ?>img/favicon/favicon.ico" /> -->
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap" rel="stylesheet">
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/fonts/fontawesome.css" />
+    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/fonts/tabler-icons.css" />
+    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/fonts/flag-icons.css" />
+
+    <!-- Core CSS -->
+
+    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/css/rtl/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/css/rtl/theme-bordered.css" class="template-customizer-theme-css" />
+
+    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>css/demo.css" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/node-waves/node-waves.css" />
+
+    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/typeahead-js/typeahead.css" />
+    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/apex-charts/apex-charts.css" />
+    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/swiper/swiper.css">
+    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/datatables-bs5/datatables.bootstrap5.css">
+    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css">
+    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css">
+    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/libs/leaflet/leaflet.css">
+
+
+    <!-- Page CSS -->
+    <link rel="stylesheet" href="<?= base_url('vue_assets/'); ?>vendor/css/pages/cards-advance.css" />
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <!-- Helpers -->
+    <script src="<?= base_url('vue_assets/'); ?>vendor/js/helpers.js"></script>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+
+    <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
+    <!-- <script src="<?= base_url('vue_assets/'); ?>vendor/js/template-customizer.js"></script> -->
+
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="<?= base_url('vue_assets/'); ?>js/config.js"></script>
